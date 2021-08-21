6152decf64f0c32c37335435004cb6c32309e278
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-08-21 21:32:43 +0100

    Rewrite to make environment same as tutorial

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,23 +1,20 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
-
 export class ButtonComponent implements OnInit {
-  @Input() text: string | undefined;
-  @Input() color: string | undefined;
-  @Output() btnClick = new EventEmitter()
+  @Input() text: string;
+  @Input() color: string;
+  @Output() btnClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onClick() {
     this.btnClick.emit();
   }
-
 }
